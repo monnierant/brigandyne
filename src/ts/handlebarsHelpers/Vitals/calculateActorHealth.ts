c494d5a67d6c0dbae45aeb8e2548e6a473fe73a1
Monnier Antoine <monnier.ant@free.fr>
2024-10-16 22:37:25 +0200

    fix: Fix issue caused by breaking change in tsc 5.6.3

---
+++ src/ts/handlebarsHelpers/Vitals/calculateActorHealth.ts
@@ -0,0 +1,28 @@
+import BrigandyneActor from "../../apps/documents/BrigandyneActor";
+import { BrigandyneActorSystem } from "../../apps/schemas/BrigandyneActorSchema";
+import { StatHelpers } from "../../apps/helpers/StatHelpers";
+import { abilities } from "../../constants";
+
+export const calculateActorHealth = function (actor: BrigandyneActor) {
+  const syst = (actor as any).system as any as BrigandyneActorSystem;
+
+  const base = StatHelpers.calculateVitality(
+    StatHelpers.calculateAbility(syst.abilities[abilities.indexOf("FOR")]),
+    StatHelpers.calculateAbility(syst.abilities[abilities.indexOf("END")]),
+    StatHelpers.calculateAbility(syst.abilities[abilities.indexOf("VOL")])
+  );
+
+  return {
+    current: syst.health.current,
+    max: base + syst.health.modificator,
+    percent: Math.round(
+      (syst.health.current / (base + syst.health.modificator)) * 100
+    ),
+    line: StatHelpers.calculateLine(base + syst.health.modificator),
+    lineValue: Math.max(
+      syst.health.current -
+        StatHelpers.calculateLine(base + syst.health.modificator),
+      0
+    ),
+  };
+};
